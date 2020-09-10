adf27e855f8306c46d59d7ba395358c3c69bc127
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-09-10 10:57:27 -0700

    Added several length validations

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,6 +4,8 @@ class Gallery < ApplicationRecord
     has_many :memories, dependent: :destroy
 
     validates :title, :description, :image, presence: true
+    validates :title, length: { maximum: 40 }
+    validates :description, length: { maximum: 385 }
     
     has_attached_file :image, styles: { :medium => "400x400#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,6 +1,6 @@
 class Memory < ApplicationRecord
     belongs_to :user
-    belongs_to :gallery #  optional: true #ADDED OPTIONAL TRUE AND CAN SAVE MEMORY NOW
+    belongs_to :gallery
     
     has_many :highlights, dependent: :destroy
     has_many :tripnotes, dependent: :destroy
@@ -11,9 +11,15 @@ class Memory < ApplicationRecord
     accepts_nested_attributes_for :tripnotes,
                                    reject_if: proc { |attributes| attributes['detail'].blank? },
                                    allow_destroy: true
+
     
     validates :title, :description, :image, presence: true
-    
+    validates :title, length: { maximum: 25 }
+    validates :description, length: { maximum: 250 }
+    validates :highlights, length: { maximum: 5, too_long: "can only contain up to %{count} bullets." }
+    validates :tripnotes, length: { maximum: 5, too_long: "can only contain up to %{count} notes." }
+
     has_attached_file :image, styles: { :medium => "400x400#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
